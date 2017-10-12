1a23066cd7ce2d23a9838a9b81b3e26c399f011f
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-10-13 02:59:57 +0530

    Script updated

--- react-16/index.js
+++ react-16/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import { FragmentsArray, FragmentsString } from './snippets/Fragments.jsx';
 // const Fragments = <div><FragmentsArray /><FragmentsString /></div>;
 //import ErrorBoundaries from './snippets/ErrorBoundaries.jsx';
-import { App } from './snippets/Portals.jsx';
-//import App from './snippets/CustomAttributes.jsx';
+//import { App } from './snippets/Portals.jsx';
+import App from './snippets/CustomAttributes.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
