078264a5e10dbb9381f8de4f5192be8e841ac3eb
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-12-01 03:35:57 +0530

    React.Fragment support

--- react-16/index.js
+++ react-16/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { FragmentsArray, FragmentsString } from './snippets/Fragments.jsx';
+import { Fragment } from './snippets/Fragments.jsx';
 // const Fragments = <div><FragmentsArray /><FragmentsString /></div>;
 //import ErrorBoundaries from './snippets/ErrorBoundaries.jsx';
 //import { App } from './snippets/Portals.jsx';
-import App from './snippets/CustomAttributes.jsx';
+//import App from './snippets/CustomAttributes.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Fragment />, document.getElementById('app'));
--- react-16/snippets/Fragments.jsx
+++ react-16/snippets/Fragments.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-const FragmentsArray = () => {
-	return [
-		<h1 key="1">This is h1</h1>,
-		<h2 key="2">This is h2</h2>,
-		<h3 key="3">This is h3</h3>
-	];
-},
-	FragmentsString = () => {
- 		return 'Just a string';
-	};
+// const FragmentsArray = () => {
+// 	return [
+// 		<h1 key="1">This is h1</h1>,
+// 		<h2 key="2">This is h2</h2>,
+// 		<h3 key="3">This is h3</h3>
+// 	];
+// },
+// 	FragmentsString = () => {
+//  		return 'Just a string';
+// 	},
 
-export { FragmentsArray, FragmentsString };
+const Fragment = () => {
+	return (
+		<React.Fragment>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+		</React.Fragment>
+	);
+};
+
+
+export { Fragment };
